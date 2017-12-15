3a9f4b16b74f5cf890d0a128b070fb87b9ad95cc
mandy donkervoort <mandy.donkervoort@hva.nl>
2017-12-15 16:44:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ function logWhisper(string) {
   return console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string) {
-  
-  if(string.toLowerCase()) {
-  return "I can't hear you!"
-  }
-  
-   if(string.toUpperCase()) {
-  return "YES INDEED!"
+function sayHiToGrandma(string){
+
+  if (string === string.toLowerCase()){
+    return "I can't hear you!"
+  }else if (string === string.toUpperCase()){
+    return "YES INDEED!"
+  }else if (string === "I love you, Grandma."){
+    return "I love you, too."
   }
 }
